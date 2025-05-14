5d82ed874ae4e90e3e2adad8171de4a3feaeb65c
simagyari <72996386+simagyari@users.noreply.github.com>
2025-05-14 21:00:49 +0100

    feat: include length for lines, precise output typespec

--- lib/geomeasure.ex
+++ lib/geomeasure.ex
@@ -127,33 +127,34 @@ defmodule GeoMeasure do
   }
 
   @type geo_point :: Geo.Point.t() | Geo.PointM.t() | Geo.PointZ.t() | Geo.PointZM.t()
+  @type geo_line :: Geo.LineString.t() | Geo.LineStringZ.t() | Geo.LineStringZM.t()
 
   @doc """
   Calculates the area of a Geo struct.
   """
   @doc since: "0.0.1"
-  @spec area(Geo.geometry()) :: number()
+  @spec area(Geo.geometry()) :: float
   defdelegate area(geometry), to: Area, as: :calculate
 
   @doc """
   Calculates the bounding box of a Geo struct.
   """
   @doc since: "0.0.1"
-  @spec bbox(Geo.geometry()) :: Geo.geometry()
+  @spec bbox(Geo.geometry()) :: Geo.Point.t() | Geo.PointZ.t() | Geo.Polygon.t()
   defdelegate bbox(geometry), to: Bbox, as: :calculate
 
   @doc """
   Calculates the centroid of a Geo struct.
   """
   @doc since: "0.0.1"
-  @spec centroid(Geo.geometry()) :: Geo.Point.t()
+  @spec centroid(Geo.geometry()) :: Geo.Point.t() | Geo.PointZ.t()
   defdelegate centroid(geometry), to: Centroid, as: :calculate
 
   @doc """
   Calculates the distance between two coordinate pairs or points.
   """
   @doc since: "0.0.1"
-  @spec distance({number(), number()}, {number(), number()}) :: float()
+  @spec distance({number(), number()}, {number(), number()}) :: float
   @spec distance(geo_point(), geo_point()) :: float()
   defdelegate distance(coordinates_1, coordinates_2), to: Distance, as: :calculate
 
@@ -161,13 +162,20 @@ defmodule GeoMeasure do
   Calculates the extent coordinates of a Geo struct.
   """
   @doc since: "0.0.1"
-  @spec extent(Geo.geometry()) :: {number(), number(), number(), number()}
+  @spec extent(Geo.geometry()) :: {number, number, number, number}
   defdelegate extent(geometry), to: Extent, as: :calculate
 
   @doc """
-  Calculates the perimeter of a Geo struct.
+  Calculates the length of a linear Geo struct.
+  """
+  @doc since: "1.3.0"
+  @spec length(geo_line()) :: float
+  defdelegate length(geometry), to: Perimeter, as: :calculate
+
+  @doc """
+  Calculates the perimeter of a polygonal Geo struct.
   """
   @doc since: "0.0.1"
-  @spec perimeter(Geo.geometry()) :: float()
+  @spec perimeter(Geo.Polygon.t()) :: float
   defdelegate perimeter(geometry), to: Perimeter, as: :calculate
 end

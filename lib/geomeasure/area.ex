7c2ff2fc21e50b858a9b1462e65e6b2bf790ad3c
simagyari <72996386+simagyari@users.noreply.github.com>
2025-05-14 21:47:31 +0100

    fix: correct wrong shoelace method implementation

--- lib/geomeasure/area.ex
+++ lib/geomeasure/area.ex
@@ -3,24 +3,24 @@ defmodule GeoMeasure.Area do
 
   alias GeoMeasure.Utils
 
-  @spec calculate_area([{number(), number()}]) :: float()
+  @spec calculate_area([{number, number}]) :: float
   defp calculate_area(coords) when is_list(coords) do
     coords
     |> Enum.reduce({0, tl(coords)}, fn item, accumulator ->
       Utils.tuple_not_nil!(item)
+      IO.inspect({item, accumulator})
       {x1, y1} = item
 
       case accumulator do
         {acc, [{x2, y2} | rest]} ->
           Utils.tuple_not_nil!({x2, y2})
-          acc = acc + abs(x1 * y2 - x2 * y1)
-          {acc, [{x2, y2}] ++ rest}
+          acc = acc + x1 * y2 - x2 * y1
+          {acc, rest}
 
-        {acc, [{_}]} ->
-          {acc}
+        {acc, []} -> acc
       end
     end)
-    |> elem(0)
+    |> abs()
     |> Kernel./(2)
   end
 
@@ -28,7 +28,7 @@ defmodule GeoMeasure.Area do
   Calculates the area of a Geo struct.
   """
   @doc since: "0.0.1"
-  @spec calculate(Geo.Polygon.t()) :: float()
+  @spec calculate(Geo.Polygon.t()) :: float
   def calculate(%Geo.Polygon{coordinates: [coords]}) do
     calculate_area(coords)
   end

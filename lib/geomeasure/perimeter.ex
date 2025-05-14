e921a44b08357cf7fe0fda3c165a8e6b027aa68e
simagyari <72996386+simagyari@users.noreply.github.com>
2025-05-14 20:51:10 +0100

    feat: handles linestrings and polygons alike

--- lib/geomeasure/perimeter.ex
+++ lib/geomeasure/perimeter.ex
@@ -1,14 +1,17 @@
 defmodule GeoMeasure.Perimeter do
   @moduledoc false
 
-  alias GeoMeasure.Distance
+  alias GeoMeasure.{Distance, Utils}
 
   @spec calculate_perimeter([{number(), number()}]) :: float()
   defp calculate_perimeter(coords) when is_list(coords) do
     coords
-    |> Enum.drop(-1)
     |> Enum.reduce({0, tl(coords)}, fn point_1, {acc, remaining} ->
+      IO.inspect({point_1, remaining, acc})
       case remaining do
+        [] ->
+          acc
+
         [point_2 = {_a, _b}] ->
           acc = acc + Distance.calculate(point_1, point_2)
           {acc, []}
@@ -18,13 +21,30 @@ defmodule GeoMeasure.Perimeter do
           {acc, rest}
       end
     end)
-    |> elem(0)
   end
 
   @doc """
   Calculates the perimeter of a Geo struct.
   """
   @doc since: "0.0.1"
+
+  @spec calculate(Geo.LineString.t()) :: float()
+  def calculate(%Geo.LineString{coordinates: coords}) do
+    calculate_perimeter(coords)
+  end
+
+  @spec calculate(Geo.LineStringZ.t()) :: float()
+  def calculate(%Geo.LineStringZ{coordinates: coords}) do
+    calculate_perimeter(coords)
+  end
+
+  @spec calculate(Geo.LineStringZM.t()) :: float()
+  def calculate(%Geo.LineStringZM{coordinates: coords}) do
+    coords
+    |> Utils.remove_m_values()
+    |> calculate_perimeter()
+  end
+
   @spec calculate(Geo.Polygon.t()) :: float()
   def calculate(%Geo.Polygon{coordinates: [coords]}) do
     calculate_perimeter(coords)

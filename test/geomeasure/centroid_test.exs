b009d476c8ccba4a1459f982a43811a91869a300
simagyari <72996386+simagyari@users.noreply.github.com>
2025-05-24 09:52:18 +0100

    feat: add linestringz and zm tests

--- test/geomeasure/centroid_test.exs
+++ test/geomeasure/centroid_test.exs
@@ -26,6 +26,16 @@ defmodule GeoMeasure.Centroid.Test do
     assert GeoMeasure.Centroid.calculate(geom) == %Geo.Point{coordinates: {2.0, 3.0}}
   end
 
+  test "calculate_linestringz_centroid" do
+    geom = %Geo.LineStringZ{coordinates: [{1, 2, 3}, {3, 4, 5}]}
+    assert GeoMeasure.Centroid.calculate(geom) == %Geo.PointZ{coordinates: {2.0, 3.0, 4.0}}
+  end
+
+  test "calculate_linestringzm_centroid" do
+    geom = %Geo.LineStringZM{coordinates: [{1, 2, 3, 10}, {3, 4, 5, 11}]}
+    assert GeoMeasure.Centroid.calculate(geom) == %Geo.PointZ{coordinates: {2.0, 3.0, 4.0}}
+  end
+
   test "calculate_polygon_centroid" do
     geom = %Geo.Polygon{coordinates: [[{0, 0}, {0, 2}, {2, 2}, {2, 0}, {0, 0}]]}
     assert GeoMeasure.Centroid.calculate(geom) == %Geo.Point{coordinates: {1.0, 1.0}}
@@ -63,6 +73,16 @@ defmodule GeoMeasure.Centroid.Test do
 
   test "calculate_linestring_centroid_nil_coord" do
     geom = %Geo.LineString{coordinates: [{1, nil}, {3, 4}]}
+    assert_raise ArgumentError, fn -> GeoMeasure.Centroid.calculate(geom) end
+  end
+
+  test "calculate_linestringz_centroid_nil_coord" do
+    geom = %Geo.LineStringZ{coordinates: [{1, 2, nil}, {3, 4, 5}]}
+    assert_raise ArgumentError, fn -> GeoMeasure.Centroid.calculate(geom) end
+  end
+
+  test "calculate_linestringzm_centroid_nil_coord" do
+    geom = %Geo.LineStringZM{coordinates: [{1, 2, 3, 10}, {3, nil, 5, 11}]}
     assert_raise ArgumentError, fn -> GeoMeasure.Centroid.calculate(geom) end
   end
 
--- test/geomeasure/perimeter_test.exs
+++ test/geomeasure/perimeter_test.exs
@@ -6,14 +6,54 @@ defmodule GeoMeasure.Perimeter.Test do
     assert_raise FunctionClauseError, fn -> GeoMeasure.Perimeter.calculate(geom) end
   end
 
-  test "calculate_linestring_perimeter" do
-    geom = %Geo.LineString{coordinates: [{1, 2}, {3, 4}]}
+  test "calculate_pointm_perimeter" do
+    geom = %Geo.PointM{coordinates: {1, 2}}
     assert_raise FunctionClauseError, fn -> GeoMeasure.Perimeter.calculate(geom) end
+  end
+
+  test "calculate_pointz_perimeter" do
+    geom = %Geo.PointZ{coordinates: {1, 2}}
+    assert_raise FunctionClauseError, fn -> GeoMeasure.Perimeter.calculate(geom) end
+  end
+
+  test "calculate_pointzm_perimeter" do
+    geom = %Geo.PointZM{coordinates: {1, 2}}
+    assert_raise FunctionClauseError, fn -> GeoMeasure.Perimeter.calculate(geom) end
+  end
+
+  test "calculate_linestring_length" do
+    geom = %Geo.LineString{coordinates: [{1, 2}, {1, 4}]}
+    assert GeoMeasure.Perimeter.calculate(geom) == 2.0
+  end
+
+  test "calculate_linestringz_length" do
+    geom = %Geo.LineStringZ{coordinates: [{1, 2, 2}, {1, 4, 2}]}
+    assert GeoMeasure.Perimeter.calculate(geom) == 2.0
+  end
+
+  test "calculate_linestringzm_length" do
+    geom = %Geo.LineStringZM{coordinates: [{1, 2, 2, 10}, {1, 4, 2, 11}]}
+    assert GeoMeasure.Perimeter.calculate(geom) == 2.0
   end
 
   test "calculate_polygon_perimeter" do
     geom = %Geo.Polygon{coordinates: [[{0, 0}, {0, 2}, {2, 2}, {2, 0}, {0, 0}]]}
     assert GeoMeasure.Perimeter.calculate(geom) == 8.0
+  end
+
+  test "calculate_linestring_length_nil_coord" do
+    geom = %Geo.LineString{coordinates: [{1, nil}, {1, 4}]}
+    assert_raise ArgumentError, fn -> GeoMeasure.Perimeter.calculate(geom) end
+  end
+
+  test "calculate_linestringz_length_nil_coord" do
+    geom = %Geo.LineStringZ{coordinates: [{1, 2, 2}, {1, nil, 2}]}
+    assert_raise ArgumentError, fn -> GeoMeasure.Perimeter.calculate(geom) end
+  end
+
+  test "calculate_linestringzm_length_nil_coord" do
+    geom = %Geo.LineStringZM{coordinates: [{nil, 2, 2, 10}, {1, 4, 2, 11}]}
+    assert_raise ArgumentError, fn -> GeoMeasure.Perimeter.calculate(geom) end
   end
 
   test "calculate_polygon_perimeter_nil_coord" do

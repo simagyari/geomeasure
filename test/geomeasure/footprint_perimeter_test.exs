bc69a336398dc5bab6807834b6cf2369784c41ee
simagyari <72996386+simagyari@users.noreply.github.com>
2026-01-27 20:48:50 +0000

    test: add tests for multipart geometry footprint perimeter

--- test/geomeasure/footprint_perimeter_test.exs
+++ test/geomeasure/footprint_perimeter_test.exs
@@ -11,6 +11,17 @@ defmodule GeoMeasure.FootprintPerimeter.Test do
     assert GeoMeasure.FootprintPerimeter.calculate(geom) == 2.0
   end
 
+  test "calculate_multilinestringz_footprint_length" do
+    geom = %Geo.MultiLineStringZ{
+      coordinates: [
+        [{0, 0, 0}, {0, 3, 0}],
+        [{0, 0, 0}, {4, 0, 0}]
+      ]
+    }
+
+    assert GeoMeasure.FootprintPerimeter.calculate(geom) == 7.0
+  end
+
   test "calculate_linestringz_footprint_length_nil_coord" do
     geom = %Geo.LineStringZ{coordinates: [{1, 2, 2}, {1, nil, 2}]}
     assert_raise ArgumentError, fn -> GeoMeasure.FootprintPerimeter.calculate(geom) end
@@ -21,9 +32,31 @@ defmodule GeoMeasure.FootprintPerimeter.Test do
     assert_raise ArgumentError, fn -> GeoMeasure.FootprintPerimeter.calculate(geom) end
   end
 
+  test "calculate_multilinestringz_footprint_length_nil_coord" do
+    geom = %Geo.MultiLineStringZ{
+      coordinates: [
+        [{0, 0, 0}, {nil, 3, 0}],
+        [{0, 0, 0}, {4, 0, 0}]
+      ]
+    }
+
+    assert_raise ArgumentError, fn -> GeoMeasure.FootprintPerimeter.calculate(geom) end
+  end
+
   test "calculate_polygonz_footprint_perimeter" do
     geom = %Geo.PolygonZ{coordinates: [[{0, 0, 0}, {0, 2, 1}, {2, 2, 2}, {2, 0, 1}, {0, 0, 0}]]}
     assert GeoMeasure.FootprintPerimeter.calculate(geom) == 8.0
+  end
+
+  test "calculate_multipolygonz_footprint_perimeter" do
+    geom = %Geo.MultiPolygonZ{
+      coordinates: [
+        [[{0, 0, 0}, {0, 2, 1}, {2, 2, 2}, {2, 0, 1}, {0, 0, 0}]],
+        [[{3, 3, 0}, {3, 5, 1}, {5, 5, 2}, {5, 3, 1}, {3, 3, 0}]]
+      ]
+    }
+
+    assert GeoMeasure.FootprintPerimeter.calculate(geom) == 16.0
   end
 
   test "calculate_polygonz_footprint_perimeter_hole" do
@@ -37,9 +70,34 @@ defmodule GeoMeasure.FootprintPerimeter.Test do
     assert GeoMeasure.FootprintPerimeter.calculate(geom) == 16.0
   end
 
+  test "calculate_multipolygonz_footprint_perimeter_hole" do
+    geom = %Geo.MultiPolygonZ{
+      coordinates: [
+        [[{0, 0, 0}, {0, 2, 1}, {2, 2, 2}, {2, 0, 1}, {0, 0, 0}]],
+        [
+          [{3, 3, 0}, {3, 5, 1}, {5, 5, 2}, {5, 3, 1}, {3, 3, 0}],
+          [{4, 4, 0}, {4, 4.5, 1}, {4.5, 4.5, 2}, {4.5, 4, 1}, {4, 4, 0}]
+        ]
+      ]
+    }
+
+    assert GeoMeasure.FootprintPerimeter.calculate(geom) == 18.0
+  end
+
   test "calculate_polygonz_footprint_perimeter_nil_coord" do
     geom = %Geo.PolygonZ{
       coordinates: [[{0, 0, 0}, {nil, 2, 0}, {2, nil, 0}, {2, 0, 0}, {0, 0, 0}]]
+    }
+
+    assert_raise ArgumentError, fn -> GeoMeasure.FootprintPerimeter.calculate(geom) end
+  end
+
+  test "calculate_multipolygonz_footprint_perimeter_nil_coord" do
+    geom = %Geo.MultiPolygonZ{
+      coordinates: [
+        [[{0, 0, 0}, {0, 2, 1}, {2, 2, 2}, {2, 0, 1}, {0, 0, 0}]],
+        [[{3, 3, 0}, {3, 5, 1}, {5, 5, 2}, {5, nil, 1}, {3, 3, 0}]]
+      ]
     }
 
     assert_raise ArgumentError, fn -> GeoMeasure.FootprintPerimeter.calculate(geom) end

b641bfdcf5bcde4934ded287f4f7fecb153df159
simagyari <72996386+simagyari@users.noreply.github.com>
2026-01-27 20:47:58 +0000

    fix: remove nonexistent multilinestringzm support

--- lib/geomeasure/footprint_perimeter.ex
+++ lib/geomeasure/footprint_perimeter.ex
@@ -17,25 +17,18 @@ defmodule GeoMeasure.FootprintPerimeter do
     |> Perimeter.calculate()
   end
 
-  @spec calculate(Geo.PolygonZ.t()) :: float
-  def calculate(polygonz = %Geo.PolygonZ{}) do
-    polygonz
-    |> Utils.polygonz_to_polygon()
-    |> Perimeter.calculate()
-  end
-
-  @spec calculate(Geo.MultiLineStringZ.t()) :: float
+  @spec calculate(Geo.MultiLineStringZ.t()) :: float()
   def calculate(%Geo.MultiLineStringZ{coordinates: multi_coords}) do
     Enum.reduce(multi_coords, 0.0, fn coords, acc ->
       acc + calculate(%Geo.LineStringZ{coordinates: coords})
     end)
   end
 
-  @spec calculate(Geo.MultiLineStringZM.t()) :: float
-  def calculate(%Geo.MultiLineStringZM{coordinates: multi_coords}) do
-    Enum.reduce(multi_coords, 0.0, fn coords, acc ->
-      acc + calculate(%Geo.LineStringZM{coordinates: coords})
-    end)
+  @spec calculate(Geo.PolygonZ.t()) :: float
+  def calculate(polygonz = %Geo.PolygonZ{}) do
+    polygonz
+    |> Utils.polygonz_to_polygon()
+    |> Perimeter.calculate()
   end
 
   @spec calculate(Geo.MultiPolygonZ.t()) :: float
